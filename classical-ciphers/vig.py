8a37ad36fc4d6305f2e304955d58f2c7552060e8
mort-yn <kimumartin.9@yahoo.com>
2026-06-08 15:53:28 +0300

    added the vigenere decryption program

--- classical-ciphers/vig.py
+++ classical-ciphers/vig.py
@@ -38,3 +38,45 @@ key = input("Enter key: ")
 ciphertext = encrypt_vigenere(plaintext, key)
 
 print("\nCiphertext:", ciphertext)
+
+
+## Vigenère Cipher Decryption
+
+def decrypt_vigenere(ciphertext, key):
+    plaintext = ""
+    key = key.upper()
+    key_index = 0
+
+    for char in ciphertext:
+
+        if char.isalpha():
+
+            shift = ord(key[key_index % len(key)]) - ord('A')
+
+            if char.isupper():
+                decrypted_char = chr(
+                    (ord(char) - ord('A') - shift) % 26
+                    + ord('A')
+                )
+            else:
+                decrypted_char = chr(
+                    (ord(char) - ord('a') - shift) % 26
+                    + ord('a')
+                )
+
+            plaintext += decrypted_char
+            key_index += 1
+
+        else:
+            plaintext += char
+
+    return plaintext
+
+
+# Main Program
+ciphertext = input("Enter ciphertext: ")
+key = input("Enter key: ")
+
+plaintext = decrypt_vigenere(ciphertext, key)
+
+print("\nPlaintext:", plaintext)
